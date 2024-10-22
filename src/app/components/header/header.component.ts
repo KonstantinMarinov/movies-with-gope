f01c11f7d5e6b5e800102a6cba865017711e613a
moka <konstantin.marinov17@gmail.com>
2024-10-22 09:30:51 +0300

    Refactored header into separate component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mwg-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    standalone: true
+})
+export class HeaderComponent {
+    
+}
