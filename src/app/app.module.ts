7104a7e4eebe61c07d11c33da9c9709a8c683351
moka <konstantin.marinov17@gmail.com>
2024-10-22 09:46:22 +0300

    Added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from "./components/footer/footer.component";
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderComponent, FooterComponent, NavigationComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mwg-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css',
+  standalone: true,
+})
+export class NavigationComponent {}
